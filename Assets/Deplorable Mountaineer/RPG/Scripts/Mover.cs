219d6e45819d46ce80accb850f967e5e2e2aeee1
DeplorableMountaineer <tdvance@deplorablemountaineer.com>
2021-07-01 09:47:59 -0400

    Clicking creates ray from camera toward click point

--- Assets/Deplorable Mountaineer/RPG/Scripts/Mover.cs
+++ Assets/Deplorable Mountaineer/RPG/Scripts/Mover.cs
@@ -5,7 +5,20 @@ namespace Deplorable_Mountaineer.RPG {
     public class Mover : MonoBehaviour {
         [SerializeField] private Transform target;
 
+        private Ray _lastRay;
+        private Camera _mainCamera;
+
+        private void Awake(){
+            _mainCamera = Camera.main;
+        }
+
         private void Update(){
+            if(Input.GetMouseButtonDown(0)){
+                _lastRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            }
+
+            Debug.DrawRay(_lastRay.origin, _lastRay.direction*100, Color.blue, 1);
+
             GetComponent<NavMeshAgent>().destination = target.position;
         }
     }
