2af3c9124c026310621f01577e49cd229e8ef9bb
DeplorableMountaineer <tdvance@deplorablemountaineer.com>
2021-07-05 10:00:10 -0400

    Walk/run animation

--- Assets/Deplorable Mountaineer/RPG/Scripts/Mover.cs
+++ Assets/Deplorable Mountaineer/RPG/Scripts/Mover.cs
@@ -2,20 +2,34 @@ using UnityEngine;
 using UnityEngine.AI;
 
 namespace Deplorable_Mountaineer.RPG {
+    [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(NavMeshAgent))]
     public class Mover : MonoBehaviour {
+        private static readonly int ForwardSpeed = Animator.StringToHash("ForwardSpeed");
+
         private Camera _mainCamera;
         private NavMeshAgent _navMeshAgent;
+        private Transform _transform;
+        private Animator _animator;
 
         private void Awake(){
+            _transform = transform;
             _mainCamera = Camera.main;
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update(){
             if(Input.GetMouseButtonDown(0)){
                 MoveToCursor();
             }
+
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator(){
+            float forwardSpeed = Vector3.Dot(_navMeshAgent.velocity, _transform.forward);
+            _animator.SetFloat(ForwardSpeed, forwardSpeed);
         }
 
         private void MoveToCursor(){
